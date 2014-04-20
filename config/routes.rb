2e0fabf8cb4196870742a9241d94e3c8699adf21
jennikins813 <jennikins813@yahoo.com>
2014-04-20 19:08:14 -0400

    Generated restaurants controller and views. Defined the 7 routes in controller file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Seatyourself1::Application.routes.draw do
+  resources :restaurants
+  
   #get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
