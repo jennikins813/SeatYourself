bf63bddd1af6ef7ac139c318820506fc357b4954
jennikins813 <jennikins813@yahoo.com>
2014-04-21 16:51:25 -0400

    Created reservation model and added associations with user,restaurant,reservation models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ActiveRecord::Base
 	validates :name, :address, :phone_number, :description, :presence => true
 	#add below validation for capacity later:
 	#validates :capacity, :numericality => {:only_integer => true}
+
+	has_many :reservations
+	has_many :users, :through => :reservations
 end
