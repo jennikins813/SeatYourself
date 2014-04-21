d92f9da49be5d5ebe72429580bcbbe898cae84da
jennikins813 <jennikins813@yahoo.com>
2014-04-21 13:45:18 -0400

    Added restaurant validations and ability to display validation errors

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, :address, :phone_number, :description, :presence => true
+	#add below validation for capacity later:
+	#validates :capacity, :numericality => {:only_integer => true}
 end
